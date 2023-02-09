4996dd142045136ce6e7cfa1eaac3262f72aa3ed
Ainamaani Isaac <aina.isaac2002@gmail.com>
2023-02-09 23:45:25 +0300

    set up

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/auth');
+
+const app = express();
+
+//set the view engine
+app.set('view engine','ejs');
+
+//middleware
+
+const dbURI = 'mongodb+srv://library:elibrary@trial.nacabxh.mongodb.net/E-Library?retryWrites=true&w=majority';
+mongoose.connect(dbURI,{ useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result)=>{
+        app.listen(3000);
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
