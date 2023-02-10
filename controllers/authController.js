3a09b6a5c0f69af5c5432e430764c4248373eefd
Ainamaani Isaac <aina.isaac2002@gmail.com>
2023-02-11 00:44:34 +0300

    added model and mongoose hooks

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,26 @@
+const User = require('../models/user');
+
+//handle errors
+const handleErrors = (err) =>{
+    console.log(err.message, err.code);
+    let errors = { email: '', password: '' };
+
+    //duplicate error code
+    if(err.code === 11000){
+        errors.email = 'that email is already registered';
+        return errors;
+    }
+
+    //validation errors
+    if(err.message.includes('userr validation failed')){
+        Object.values(err.errors).forEach(({properties})=>{
+            errors[properties.path] = properties.message;
+        });
+    }
+
+    return errors;
+}
+
 module.exports.home = (req,res) => {
     res.render('home');
 }
@@ -16,8 +39,13 @@ module.exports.signupGet = (req,res) =>{
     res.render('signup');
 }
 
-module.exports.signupPost = (req,res) =>{
+module.exports.signupPost = async (req,res) =>{
     const { email, password } = req.body;
-    console.log(email, password);
-    res.send('Signing Up')
+    try {
+        const user = await User.create({ email, password });
+        res.status(201).json(user);
+    } catch (err) {
+        const errors = handleErrors(err)
+        res.status(400).json({errors});
+    }
 }
--- models/user.js
+++ models/user.js
@@ -1,17 +1,35 @@
 const mongoose = require('mongoose');
+const { isEmail } = require('validator');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
-        required: true,
+        required: [true, 'Please enter an email'],
         unique: true,
-        lowercase: true
+        lowercase: true,
+        validate: [isEmail,'Please enter a valid email']
     },
     password: {
         type: String,
-        required: true,
-        minLength: 6
+        required: [true, 'Please enter a password'],
+        minlength: [6,'Password should have a minimum of 6 characters']
     }
+})
+
+//Mongoose hooks
+//Fire a function after the doc is saved
+userSchema.post('save',function (doc,next){
+    console.log('user has just beenn saved in db', doc);
+    next();
+})
+
+//Fire a function just before the doc is saved
+userSchema.pre('save', async function(next){
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password,salt);
+    console.log('user about to be saved in db',this) //this refers to the local instance of the user before saving it to the db
+    next();
 })
 
 const User = mongoose.model('userr',userSchema);
